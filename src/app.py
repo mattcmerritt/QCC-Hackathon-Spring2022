3ed63a29808db3105ad9a7a92d91015b36e029f2
Ty Hutchison <hutch445@gmail.com>
2022-04-09 11:28:33 -0400

    corrected tts pathing, and how it plays

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from gtts import gTTS
+from playsound import playsound
 import os
 
-currentString = 'Please dear god let this work'
+currentString = 'Please dear god let this work. This is another sentence, and I have a lot more to say. You had better keep listening or I will be very upset.'
 
 language = 'en'
 
@@ -9,4 +10,5 @@ myTTS = gTTS(text = currentString, lang = language, slow = False)
 
 myTTS.save("currentString.mp3")
 
-os.system("currentString.mp3")
+path = os.getcwd() + '\\currentString.mp3'
+playsound(path)
