255079ac254706a8cbc629be628b35875146a726
mattcmerritt <mattcmerritt@gmail.com>
2022-04-09 13:17:21 -0400

    added pause and play window

--- src/app.py
+++ src/app.py
@@ -3,8 +3,10 @@ from pygame import mixer
 import os
 import time
 
-from PIL import Image
 import pytesseract
+
+from tkinter import *
+from PIL import Image, ImageTk
 
 debug = True
 
@@ -19,7 +21,7 @@ pytesseract.pytesseract.tesseract_cmd = dir
 mixer.init()
 
 # code for playing the sound
-def readAloud(currentString):
+def read_aloud(currentString):
     myTTS = gTTS(text = currentString, lang = 'en', slow = False)
 
     myTTS.save(r"files\currentString.mp3")
@@ -29,25 +31,50 @@ def readAloud(currentString):
     mixer.music.load(path)
     mixer.music.play()
 
-    time.sleep(1)
-    mixer.music.pause()
-    time.sleep(1)
-    mixer.music.unpause()
-    time.sleep(1)
-    mixer.music.stop()
-    time.sleep(1)
-    mixer.music.play()
-
 # code for reading characters from image to string
-def readImage(imgPath, isParagraph=True):
-    imgText = pytesseract.image_to_string(Image.open(imgPath))
+def read_image(img_path, is_paragraph=True):
+    img_text = pytesseract.image_to_string(Image.open(img_path))
 
-    if isParagraph:
+    if is_paragraph:
         # removing newline characters for better reading sound
-        imgText = imgText.replace('\n', '')
+        img_text = img_text.replace('\n', '')
 
-    return imgText
+    return img_text
 
-# print(readImage(r"files\demoImage.png"))
-# print(readImage(r"files\demoImage2.png"))
-readAloud(readImage(r"files\demoImage.png"))
+def create_window():
+    window = Tk()
+
+    pause_button = Button(window, text = "Pause")
+
+    pause_button.place(x = 100, y = 60)
+
+    pause_raw = Image.open(os.getcwd() + r"\images\pause.png")
+    play_raw = Image.open(os.getcwd() + r"\images\play.png")
+
+    pause_resized = pause_raw.resize((100, 100), Image.ANTIALIAS)
+    play_resized = play_raw.resize((100, 100), Image.ANTIALIAS)
+
+    pause_img = ImageTk.PhotoImage(pause_resized)
+    play_img = ImageTk.PhotoImage(play_resized)
+
+    # pause_img = PhotoImage(file=os.getcwd() + r"\images\pause.png")
+    # play_img = PhotoImage(file=os.getcwd() + r"\images\play.png")
+
+    pause_button.config(image=pause_img)
+
+    pause_button.bind('<Button-1>', lambda onclick : pause(pause_button, pause_img, play_img))
+
+    window.title('Text to Speech Reader')
+    window.geometry("300x200+10+20")
+    window.mainloop()
+
+def pause(button, pause_img, play_img):
+    if (mixer.music.get_busy()):
+        mixer.music.pause()
+        button.config(image=play_img)
+    else:
+        mixer.music.unpause()
+        button.config(image=pause_img)
+
+read_aloud(read_image(r"files\demoImage.png"))
+create_window()
