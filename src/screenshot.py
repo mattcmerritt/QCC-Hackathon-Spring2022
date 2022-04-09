f71346af9e4f305a251ada0dd96890ba7327c39e
mikepmerritt <mikepmerritt@gmail.com>
2022-04-09 14:26:33 -0400

    tried adding selection code

--- src/screenshot.py
+++ src/screenshot.py
@@ -1,9 +1,13 @@
+from contextlib import nullcontext
 from tkinter import *
 from PIL import Image, ImageTk
 from threading import Thread
 import pyautogui
 import time
     
+topleft = None
+bottomright = None
+
 def screenshot(label):
     time.sleep(5)
     image = pyautogui.screenshot()
@@ -31,18 +35,32 @@ def create_window():
 def create_image_window():
 
     original = Image.open("screenshot.png")
-
-    img = ImageTk.PhotoImage(original.resize((int(original.width/2), int(original.height/2)), Image.ANTIALIAS))
+    small = original.resize((int(original.width/2), int(original.height/2)), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(small)
 
     window = Toplevel()
 
     window.title('Screenshot')
-    window.geometry(str(int(original.width/2)) + "x" + str(int(original.height/2)) + "+30+40")
+    window.geometry(str(int(original.width/2)) + "x" + str(int(original.height/2)) + "+10+20")
+    window.bind('<Button-1>', lambda onclick : set_top_left(window))
+    window.bind('<ButtonRelease-1>', lambda onclick : set_bottom_right_and_crop(window, small))
 
     img_label = Label(window, image=img)
     img_label.pack()
 
     window.mainloop()
+
+def set_top_left(window):
+    global topleft 
+    topleft = (window.winfo_pointerx(), window.winfo_pointery())
+    print("down")
+
+def set_bottom_right_and_crop(window, image):
+    global bottomright 
+    bottomright = (window.winfo_pointerx(), window.winfo_pointery())
+    image.crop((topleft[0], topleft[1], bottomright[0], bottomright[1])).save("cropped.png", "PNG")
+    print("up")
+
 
 
 create_window()
