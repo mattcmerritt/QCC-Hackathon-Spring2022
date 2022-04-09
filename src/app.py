871853952c99fa9e8b3b02ea05390a3fe4d5ddc4
mattcmerritt <mattcmerritt@gmail.com>
2022-04-09 11:32:31 -0400

    basic reading text from image using pytesseract

--- src/app.py
+++ src/app.py
@@ -1,12 +1,30 @@
 from gtts import gTTS
 import os
 
-currentString = 'Please dear god let this work'
+from PIL import Image
+import pytesseract
 
-language = 'en'
+debug = True
 
-myTTS = gTTS(text = currentString, lang = language, slow = False)
+# setting up pytesseract with executable
+if (debug):
+    dir = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+else:
+    dir = input("Please input the path to the Tesseract OCR executable: ")
+pytesseract.pytesseract.tesseract_cmd = dir
 
-myTTS.save("currentString.mp3")
 
-os.system("currentString.mp3")
+# code for playing the sound
+def readAloud(currentString):
+    myTTS = gTTS(text = currentString, lang = 'en', slow = False)
+
+    myTTS.save(r"files\currentString.mp3")
+
+    os.system(r"files\currentString.mp3")
+
+def readImage(imgPath):
+    return pytesseract.image_to_string(Image.open(imgPath))
+
+# print(readImage(r"files\demoImage.png"))
+# print(readImage(r"files\demoImage2.png"))
+readAloud(readImage(r"files\demoImage.png"))
